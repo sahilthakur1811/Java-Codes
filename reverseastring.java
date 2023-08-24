5c721c660724b07b660a49127addce462a71096d
Sahil Thakur <103377960+sahilthakur1811@users.noreply.github.com>
2023-08-24 18:43:51 +0530

    Create reverseastring.java

---
+++ reverseastring.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class HelloWorld {
+    public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
+      String str=sc.nextLine();
+      String rev=" ";
+      
+     String s[]=str.split(" ");
+     
+     for(int i=0;i<s.length;i++){
+         rev=s[i]+" "+rev;
+     }
+     
+      System.out.print(rev);
+      
+    }
+}
